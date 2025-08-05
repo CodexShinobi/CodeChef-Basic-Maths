e3c6547d7ec5b2448265573554a238681833fafa
Navneet Prakash <142720492+CodexShinobi@users.noreply.github.com>
2025-08-05 14:55:46 +0530

    Create Fitness.cpp

---
+++ Fitness.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        // your code goes here
+        int ans=x*10;
+        cout<<ans<<endl;
+        
+    }
+}
